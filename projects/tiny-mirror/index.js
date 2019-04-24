af1f25f2e4f1beaf42d87ac28ac079723a68955c
Devon Le Duke <norosoboshi@gmail.com>
2019-04-24 19:16:17 +0200

    for youtube

--- projects/tiny-mirror/index.js
+++ projects/tiny-mirror/index.js
@@ -1,12 +1,8 @@
-// Handle FF
-navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia;
 
-window.onload = () => {
     // Create favicon link element
     const favicon = document.createElement('link');
     favicon.rel = 'shortcut icon';
     favicon.type = 'image/png';
-    favicon.href = '../../images/favicon.ico';
     document.getElementsByTagName('head')[0].appendChild(favicon);
     // Create hidden canvas
     const w = 32;
@@ -19,40 +15,21 @@ window.onload = () => {
     // Grab canvas context
     const ctx = canvas.getContext('2d');
     // Create hidden video element
-    const video = document.createElement('video');
-    video.style = 'display: none';
-    video.width = canvas.width;
-    video.height = canvas.height;
-    document.body.appendChild(video);
-    // Assign user media to video and start loop
-    navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
-        video.srcObject = stream;
-        video.play();
-        loop();
-    });
-    // Flag for mirror image
-    let mirror = false;
+    const video = document.querySelector('#movie_player > div.html5-video-container > video');
     // Loop forever
     const loop = () => {
         // save transform
         ctx.save();
-        // Mirror image based on checkbox
-        if (mirror) {
-            ctx.translate(canvas.width, 0);
-            ctx.scale(-1, 1);
-        }
-        // Copy video to canvas
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        // Copy video to canvas   width: 1198px; height: 674px;
+        const margin = (1198 - 674) / 2;
+        ctx.drawImage(video, margin, 0, 674, 674, 0, 0, canvas.width, canvas.height);
         // restore transform
         ctx.restore();
         // Set canvas to favicon
         favicon.setAttribute('href', canvas.toDataURL());
         // Loop
-        setTimeout(loop, 100);
+        setTimeout(loop, 1000/24);
     };
-    // Handle checkbox change event
-    document.getElementById('mirror').addEventListener('change', e => {
-        mirror = e.target.checked;
-    });
-};
+
+    loop();
 
